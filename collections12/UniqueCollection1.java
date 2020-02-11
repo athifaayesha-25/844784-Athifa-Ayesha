74a7b10e6794f9e2a41364b2226385341a1d2c74
Athifa Ayesha <athifaayesha2@gmail.com>
2020-02-11 14:04:16 +0530

    Add files via upload

---
+++ collections12/UniqueCollection1.java
@@ -0,0 +1,24 @@
+
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class UniqueCollection1 {
+    public static void main(String args[]) {
+        // Creating a TreeSet with a custom Comparator (Case Insensitive Order)
+        SortedSet<String> fruits = new TreeSet<>(String.CASE_INSENSITIVE_ORDER);
+
+        
+        // Adding new elements to a TreeSet
+        fruits.add("Banana");
+        fruits.add("Apple");
+        fruits.add("Pineapple");
+        fruits.add("Orange");
+
+        System.out.println("Fruits Set : " + fruits);
+
+        // Now, lowercase elements will also be considered as duplicates
+        fruits.add("banana");
+        System.out.println("After adding \"banana\" : " + fruits);
+    }
+}
