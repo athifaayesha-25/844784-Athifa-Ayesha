e6719686b2bae214716a5f4786d5ed446552495f
Athifa Ayesha <athifaayesha2@gmail.com>
2020-02-21 15:34:41 +0530

    Add files via upload

---
+++ PhoneBook.java
@@ -0,0 +1,16 @@
+package codingPackage.demo;
+
+public class PhoneBook {
+	String Name;
+	String PhoneNo;
+	
+	public PhoneBook(String Namme,String PhoneNo){
+		this.Name=Name;
+		this.PhoneNo=PhoneNo;
+	}
+	public String toString(){
+    	return "PhoneBook["+Name+" "+PhoneNo+" ]";
+	}
+
+}
+
